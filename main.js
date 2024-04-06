7ab1c70dd642528ad1a9b589e2d78142156b19a4
Al <alsami1209@gmail.com>
2024-04-06 19:38:00 +0600

    first time on async

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
 // document.getElementById('try').addEventListener("click", function(){
 //     this.style.backgroundColor = "green";
 // })
+
+// learning async
+
+function getdata(dataId) {
+    setTimeout(() =>{
+        console.log("data", dataId);
+    }, 3000);
+}

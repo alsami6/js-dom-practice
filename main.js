b26dad47aab44b847c6e7cd24a8e9faf87565830
Al <alsami1209@gmail.com>
2024-04-07 16:45:32 +0600

    learn about function(e)

--- main.js
+++ main.js
@@ -4,8 +4,17 @@
 
 // learning async
 
-function getdata(dataId) {
-    setTimeout(() =>{
-        console.log("data", dataId);
-    }, 3000);
-}
+// function getdata(dataId) {
+//     setTimeout(() =>{
+//         console.log("data", dataId);
+//     }, 3000);
+// }
+
+
+const btn = document.getElementById("try")
+
+btn.addEventListener("click", function(e){
+    if(e.ctrlKey){
+        console.log("Yeah it's work");
+    }
+})

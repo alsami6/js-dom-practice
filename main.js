34f8720956bc4f7065913f62754e9ddb0106cb12
Al <alsami1209@gmail.com>
2024-04-14 14:21:56 +0600

    Learn about JSON

--- main.js
+++ main.js
@@ -57,18 +57,32 @@
 // console.log(phone);
 
 
-const products = [
-    {id:1, name:"lenovo", price: 65000},
-    {id:2, name:"dell", price: 45000},
-    {id:3, name:"hp", price: 40000},
-    {id:4, name:"mac", price: 150000}
-]
+// const products = [
+//     {id:1, name:"lenovo", price: 65000},
+//     {id:2, name:"dell", price: 45000},
+//     {id:3, name:"hp", price: 40000},
+//     {id:4, name:"mac", price: 150000}
+// ]
 
-const names = products.map(product => product.name)
-console.log(names);
+// const names = products.map(product => product.name)
+// console.log(names);
 
-const prices = products.map(p => p.price);
-console.log(prices);
+// const prices = products.map(p => p.price);
+// console.log(prices);
 
-const expensive = products.filter(p => p.price > 50000)
-console.log(expensive);
+// const expensive = products.filter(p => p.price > 50000)
+// console.log(expensive);
+
+// const affordable = products.find(p => p.price < 50000)
+// console.log(affordable);
+
+
+// learn json
+
+const user = {
+    id: 1,
+    name: "Groib amir",
+    job: "Actor"
+}
+
+console.log(user);

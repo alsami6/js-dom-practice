90edc8fac05d3bba3da828910347ce2f04aea487
Al <alsami1209@gmail.com>
2024-04-22 17:47:27 +0600

    practicig

---
+++ append.js
@@ -0,0 +1,20 @@
+const placesList = document.getElementById("places-list")
+const li = document.createElement('li');
+li.innerText = 'Dhaka';
+placesList.appendChild(li);
+
+
+
+// const mainContainer = document.getAnimations('main-container');
+
+// const section = document.createElement('section');
+// const h1 = document.createElement('h1');
+// h1.innerText = "Hey It's worked";
+// section.appendChild(h1);
+
+// const ul = document.createElement('ul')
+// const li1 = document.createElement('li');
+// li.innerText = "tar por bolo kmn aso";
+// ul.appendChild = (li1);
+
+// mainContainer.appendChild(section);

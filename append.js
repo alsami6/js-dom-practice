46b6de72e6b8a42a8b04d10f452235711c4ae46a
Al <alsami1209@gmail.com>
2024-04-23 16:23:29 +0600

    made a commet system

--- append.js
+++ append.js
@@ -1,24 +1,37 @@
-const click = document.getElementById('clicked');
-click.addEventListener('click', ()=>{
-    const hobe = document.getElementById('changehobe');
-    hobe.innerText = "aije hoise"
-})
+// const click = document.getElementById('clicked');
+// click.addEventListener('click', ()=>{
+//     const hobe = document.getElementById('changehobe');
+//     hobe.innerText = "aije hoise"
+// })
 
 
-const another = document.getElementById('hereIsAnother');
-another.addEventListener('click', ()=>{
-    const hobe = document.getElementById('changehobe');
-    hobe.innerText = "aije koto bar hobe na"
-    hobe.style.backgroundColor = 'red';
-})
+// const another = document.getElementById('hereIsAnother');
+// another.addEventListener('click', ()=>{
+//     const hobe = document.getElementById('changehobe');
+//     hobe.innerText = "aije koto bar hobe na"
+//     hobe.style.backgroundColor = 'red';
+// })
 
 
 
-const update = document.getElementById('btn-update');
-update.addEventListener('click', ()=>{
-    const blogs = document.getElementById('blog-post');
-    const jk = blogs.value;
-    const ok = document.getElementById('fine');
-    ok.innerText = jk;
-    blogs.value = '';
+// const update = document.getElementById('btn-update');
+// update.addEventListener('click', ()=>{
+//     const blogs = document.getElementById('blog-post');
+//     const jk = blogs.value;
+//     const ok = document.getElementById('fine');
+//     ok.innerText = jk;
+//     blogs.value = '';
+// })
+
+
+
+const post = document.getElementById('c-post');
+post.addEventListener('click', ()=>{
+    const cmt = document.getElementById('comment')
+    const jk = cmt.value;
+    const commentSpace = document.getElementById('cmnt-box');
+    const p = document.createElement('p');
+    p.innerText = jk;
+    commentSpace.appendChild(p);
+    cmt.value = '';
 })

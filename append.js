2e50f9c7164223423c171ac6efaeda8ec36d8d23
Al <alsami1209@gmail.com>
2024-04-23 15:23:43 +0600

    my thought clear about event

--- append.js
+++ append.js
@@ -3,3 +3,22 @@ click.addEventListener('click', ()=>{
     const hobe = document.getElementById('changehobe');
     hobe.innerText = "aije hoise"
 })
+
+
+const another = document.getElementById('hereIsAnother');
+another.addEventListener('click', ()=>{
+    const hobe = document.getElementById('changehobe');
+    hobe.innerText = "aije koto bar hobe na"
+    hobe.style.backgroundColor = 'red';
+})
+
+
+
+const update = document.getElementById('btn-update');
+update.addEventListener('click', ()=>{
+    const blogs = document.getElementById('blog-post');
+    const jk = blogs.value;
+    const ok = document.getElementById('fine');
+    ok.innerText = jk;
+    blogs.value = '';
+})

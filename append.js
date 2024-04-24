e7cea2aa9f8af98816a372cfc01b9ebf19a703a9
Al <alsami1209@gmail.com>
2024-04-25 02:00:44 +0600

    leraning about fetching data

--- append.js
+++ append.js
@@ -47,3 +47,20 @@ function loadUsers(){
 function displayUsers(d){
     console.log(d);
 }
+
+function loadUsers2(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data => displayUsers2(data))
+}
+
+function displayUsers2(data){
+    const ul = document.getElementById('usersList')
+    for(const user of data){
+        console.log(user.name);
+        
+    const li = document.createElement('li')
+    li.innerText = user.name;
+    ul.appendChild(li)
+    }
+}
